f5c7187a1a6060d5634c2d4ace7510cc2bbe3afd
Michael Manley <michaelmanley@Michaels-MacBook-Pro.local>
2022-08-18 13:44:44 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('HOME')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
