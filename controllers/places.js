5a03711a7197526786318027f4a5b288dce10de4
Michael Manley <michaelmanley@Michaels-MBP.lan>
2022-08-18 22:05:01 -0400

    homepage update

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,18 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/h-thai-ml-tables.jpg'
+        //Photo by <a href="https://unsplash.com/@zahirnamane?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Zahir Namane</a> on <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+  
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/coffee-cat.jpg'
+        // Photo by <a href="https://unsplash.com/@nate_dumlao?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nathan Dumlao</a> on <a href="https://unsplash.com/s/photos/coffee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
       }]
       
     res.render('places/index', { places })
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,17 +4,29 @@ const Def = require('../default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${place.id}`} >
+            {place.name}
+            </a>
+          </h2>
+          <p className="text-left">
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name} width="200" height="200"/>
+          <p className="text-left">
+            Located in {place.city}, {place.state}
+            </p>
         </div>
       )
     })
     return (
         <Def>
             <main>
-                <h1>PLACES INDEX PAGE</h1>
-                {placesFormatted}
+                <h1>Places to Rant and Rave About</h1>
+                <div className="row">
+                  {placesFormatted}
+                </div>
             </main>
         </Def>
     )
