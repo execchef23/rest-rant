f6cdd110f83dbf6b064793dbe860ced58f49c3d9
Michael Manley <michaelmanley@Michaels-MacBook-Pro.local>
2022-08-25 15:24:48 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -6,6 +6,7 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
@@ -22,29 +23,21 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id] })
+  }
+})
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml-tables.jpg'
-        //Photo by <a href="https://unsplash.com/@zahirnamane?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Zahir Namane</a> on <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-  
-  
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-        // Photo by <a href="https://unsplash.com/@nate_dumlao?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nathan Dumlao</a> on <a href="https://unsplash.com/s/photos/coffee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-  
-      }]
-      
-    res.render('places/index', { places })
-  })
+
+
   
 
 module.exports = router
