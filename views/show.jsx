e9a4fe074ceb411edab2e58c1023a462fad1ca2a
Michael Manley <michaelmanley@Michaels-MacBook-Pro.local>
2022-08-25 16:25:06 -0400

    Delete implemented

--- views/show.jsx
+++ views/show.jsx
@@ -1,14 +1,34 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
+    let rating = (
+        <h3 className="inactive">
+          Currently Unrated
+        </h3>
+    )
+    let comments = (
+        <h3 className="inactive">
+            No comments yet
+        </h3>
+    )
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+          <h1>{ data.place.nameS } </h1>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+            </a>  
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form>     
           </main>
         </Def>
     )
 }
+
+        
 
 module.exports = show
